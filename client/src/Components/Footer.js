59502f49963316b5777a871dd4f36e83980ea4bf
Ermias <81765649+jemi2k@users.noreply.github.com>
2024-06-02 12:31:48 -0700

    Update Footer.js
    year 2024

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -79,7 +79,7 @@ function Footer() {
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2023 All Rights Reserved.
+              © 2023/2024 All Rights Reserved.
             </span>
             <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a
